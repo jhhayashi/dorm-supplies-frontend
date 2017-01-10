8d0c9139926d227a8c27bd445e16ffa0f6d1a1d9
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-10 00:23:39 -0500

    Add test data

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 const request = require('request');
 const config = require('../app/models/config');
+const testData = require('../app/models/testData');
 
 router.get('/', (req, res, next) => {
-    request.get(config.apiUrl + '/items', (err, response, body) => {
-        if (!err && response.statusCode == 200)
-            return res.render('index', {items: JSON.parse(body)});
-        else return res.render('index', {items: []});
-    });
+    return res.render('index', {items: testData});
 });
 
 module.exports = router;
