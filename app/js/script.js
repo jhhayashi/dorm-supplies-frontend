5091957b32f11dc6793f275951b3ff4f229ba6a1
Jordan Hayashi <jhhayashi@gmail.com>
2017-01-10 23:11:05 -0500

    Add basic example with fetch and reverse proxy

--- app/js/script.js
+++ app/js/script.js
@@ -6,3 +6,45 @@ document.body.onclick = function(e) {
 
 function showModal() { modal.style.display = 'block'; }
 function hideModal() { modal.style.display = ''; }
+
+function submitItem() {
+    var form = document.forms[0];
+    // validation goes here
+    
+    var data = {
+        name: form.name.value,
+        img: form.img.value,
+        price: form.price.value
+    };
+    if (form.description.value)
+        data.description = form.description.value;
+    if (form.inventory.value)
+        data.inventory = form.inventory.value;
+
+    // use fetch() to submit a post request to our reverse proxy
+    fetch('/items', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+    // check to make sure the request was okay. if so, extract the text from
+    // the response body
+    .then(function(res) {
+        if (!res.ok) console.log('Error!');
+        return res.text();
+    })
+    // take the response body and replace the current body with it. the response
+    // body is the rendered pug template with either "Success" or "Error" as the
+    // title
+    .then(function(res) {
+        console.log('Success!');
+        document.body.innerHTML = res;
+    })
+    // console.log any error if they occur
+    .catch(function(err) {
+        console.log('Error!');
+        console.log(err);
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,31 @@ const config = require('../app/models/config');
 const testData = require('../app/models/testData');
 
 router.get('/', (req, res, next) => {
-    return res.render('index', {items: testData});
+    request.get({url: config.apiUrl + '/items'}, function(err, r, items) {
+        if (err || r.statusCode != 200)
+            return res.render('index', { items: [] });
+        else return res.render('index', { items: JSON.parse(items) });
+    });
+});
+
+// form for adding items
+router.get('/items', function(req, res, next) {
+    return res.render('items');
+});
+
+// example reverse proxy endpoint for form submit
+router.post('/items', function(req, res, next) {
+    // could do validation here
+    
+    request.post({
+        url: config.apiUrl + '/items',
+        form: req.body
+    }, function(err, r, body) {
+        if (err || r.statusCode !== 200)
+            return res.render('items', { error: true });
+        else
+            return res.render('items', { success: true });
+    });
 });
 
 module.exports = router;
